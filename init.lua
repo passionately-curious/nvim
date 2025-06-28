5451e8bb7b17823a48b33dacf0d013e5e29be1e9
Kishan <novasuper11@proton.me>
2025-06-28 16:50:13 +0530

    Restructure

--- init.lua
+++ init.lua
@@ -31,16 +31,16 @@ vim.g.maplocalleader = ' '
 vim.g.have_nerd_font = true
 
 -- [[ Setting options ]]
-require 'options'
+require('config.options')
 
 -- [[ Basic Keymaps ]]
-require 'keymaps'
+require('config.keymaps')
 
 -- [[ Install `lazy.nvim` plugin manager ]]
-require 'lazy-bootstrap'
+require('config.lazy-bootstrap')
 
 -- [[ Configure and install plugins ]]
-require 'lazy-plugins'
+require('config.lazy-plugins')
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
