02a5c5047c605ee995abb7824c55ccb2b6cdf24a
Kishan <novasuper11@proton.me>
2025-07-14 15:38:53 +0530

    Modularize lazy-plugins.lua

--- init.lua
+++ init.lua
@@ -31,16 +31,19 @@ vim.g.maplocalleader = ' '
 vim.g.have_nerd_font = true
 
 -- [[ Setting options ]]
-require('config.options')
+require 'config.options'
 
 -- [[ Basic Keymaps ]]
-require('config.keymaps')
+require 'config.keymaps'
 
 -- [[ Install `lazy.nvim` plugin manager ]]
-require('config.lazy-bootstrap')
+require 'config.lazy-bootstrap'
 
 -- [[ Configure and install plugins ]]
-require('config.lazy-plugins')
+require 'config.lazy-plugins'
+
+-- spaces -> tab
+vim.o.expandtab = false
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
